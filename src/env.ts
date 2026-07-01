3ce3ee4c0f93d4f891c02bdfa6e0e093ee8ed116
Ahmed Elamin <ahmed@elamino.dev>
2026-07-01 21:28:58 +0400

    Add typed env with @t3-oss/env

---
+++ src/env.ts
@@ -0,0 +1,18 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import * as z from "zod";
+
+export const env = createEnv({
+  server: {
+    NODE_ENV: z
+      .enum(["development", "test", "production"])
+      .default("development"),
+  },
+  client: {
+    NEXT_PUBLIC_APP_URL: z.url(),
+  },
+  experimental__runtimeEnv: {
+    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+  },
+  skipValidation: !!process.env.SKIP_ENV_VALIDATION,
+  emptyStringAsUndefined: true,
+});
